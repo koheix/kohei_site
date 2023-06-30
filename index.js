1d6dee53ed41a4b935e64798d8380d9aa0753ac9
k.takami <k.takami@rlg.sys.es.osaka-u.ac.jp>
2023-06-30 23:54:49 +0900

    update: high and lowのゲームを追加

--- index.js
+++ index.js
@@ -89,8 +89,18 @@ var cards_pic_name = [
     "card_spade_11.png",
     "card_spade_12.png",
     "card_spade_13.png",
-    "card_joker.png"
+    "card_joker100.png"
 ]
+
+// //要素がすべて0のcard_pics_name配列の長さの配列
+// //カードが使われたかどうかを判定するために使用
+// //使用されると1になる
+// var cards = new Array(cards_pic_name.length).fill(0);
+
+//現在のカードの種類の名前を格納する変数
+var current_card_name = "";
+// 現在のカード番号を格納する変数
+var current_number = 0;
 
 //connection
 io.on(
@@ -101,6 +111,8 @@ io.on(
         socket.on(
             'disconnect', () =>{
                 console.log( 'disconnect' );
+                //playerの人数を減らす
+                cnt--;
             } 
         );
 
@@ -109,22 +121,111 @@ io.on(
                 //名前を記録
                 player[socket.id] = player_name;
                 console.log(player[socket.id]);
-                //ターンを決める
-                if(cnt == 0){
-                    isTurn[socket.id] = 1;
-                    cnt++;
-                }
-                else{
-                    isTurn[socket.id] = 0;
+                //プレイヤーが二人集まったら何もしない
+                if(cnt != 2){
+                    //ターンを決める
+                    if(cnt == 0){
+                        isTurn[socket.id] = 1;
+                        cnt++;
+                        //一人目の時
+                        //現在のカードの種類を決める
+                        current_card_name = cards_pic_name[Math.floor(Math.random() * cards_pic_name.length)];
+                        // 現在のカードの数字を格納しておく
+                        current_number = parseInt(current_card_name.match(/\d+/g)[0], 10);
+                        //現在のカードの種類を配列から削除する
+                        cards_pic_name.splice(cards_pic_name.indexOf(current_card_name), 1);
+                        //ターンであることを知らせる
+                        io.to(socket.id).emit("HAL_turn_change", true);
+                    }
+                    else{
+                        cnt++;
+                        isTurn[socket.id] = 0;
+                    }
+                    //HTML上のid = current_cardの画像を変更するため、クライアントに現在のカードの種類を送信する
+                    io.to(socket.id).emit("HAL_current_card_name", current_card_name);
                 }
             }
         );
         
+        //"HAL_pushed"socketが受信されたときの処理
+        //まず、プレイヤーが二人集まっているかを確認する
+        //そうであれば以下を実行する
         //socket.idのプレイヤーのターンかどうかを確認し、そうであれば以下を実行する
-        //highボタンがクリックされると次に表示されるトランプの数字と比較して、その前のトランプの数字より大きければ正解、小さければ不正解とする
+        //current_card_nameを変更する
+        //highボタンがクリックされると
+        //そのトランプの数字と比較して、その前のトランプの数字より大きければ正解、小さければ不正解とする
+        //lowボタンがクリックされると
+        //そのトランプの数字と比較して、その前のトランプの数字より小さければ正解、大きければ不正解とする
         //正解の場合は正解と画面に表示し、不正解の場合は不正解と画面に表示する
-        //その後、次のトランプを表示する
-        
-
+        //その後、次のトランプを決定する
+        //最後にもう一方のプレイヤーのターンにする
+        //socket.idのプレイヤーのターンでなければ、その旨を画面に表示する
+        socket.on(
+            "HAL_pushed", (pushed_button)=>{
+                //プレイヤーが二人集まっているかを確認
+                if(cnt == 2){
+                    //ターンであるかを確認
+                    if(isTurn[socket.id] == 1){
+                        //正解かどうかを判定するための変数
+                        var isCorrect = 0;
+                        //next_card_nameを決定する
+                        let next_card_name = cards_pic_name[Math.floor(Math.random() * cards_pic_name.length)];
+                        //次のカードの種類を配列から削除する
+                        cards_pic_name.splice(cards_pic_name.indexOf(next_card_name), 1);
+                        //次のカードの数字を、ファイル名から取得する
+                        let number = parseInt(next_card_name.match(/\d+/g)[0], 10);
+                        //highボタンがクリックされたとき
+                        if(pushed_button == "high"){
+                            //正解かどうかを判定する
+                            if(number >= current_number){
+                                isCorrect = 1;
+                            }
+                        }
+                        //lowボタンがクリックされたとき
+                        else if(pushed_button == "low"){
+                            //正解かどうかを判定する
+                            if(number <= current_number){
+                                isCorrect = 1;
+                            }
+                        }
+                        //最後に次のカードを現在のカードにする
+                        current_card_name = next_card_name;
+                        //次のカード番号を現在のカード番号にする
+                        current_number = number;
+                        //ターンを交代する
+                        for (let key in isTurn) {
+                            isTurn[key] = 1 - isTurn[key];
+                        }                        
+                        // //HTML上のid = current_cardの画像を変更するため、全員に現在のカードの種類を送信する
+                        // io.sockets.emit("HAL_current_card_name", current_card_name);
+                        // // 正解不正解を知らせる
+                        // io.sockets.emit("HAL_iscorrect", isCorrect, player[socket.id]);
+                        //HAL_current_card_nameが処理された後にHAL_iscorrectが処理されるようにする
+                        (async () => {
+                            //HTML上のid = current_cardの画像を変更するため、全員に現在のカードの種類を送信する
+                            await io.sockets.emit("HAL_current_card_name", current_card_name);
+                            // 正解不正解を知らせる
+                            await setTimeout(function() {
+                                io.sockets.emit("HAL_iscorrect", isCorrect, player[socket.id]);
+                            }, 300);
+                        })();
+                        //HAL_turn_changeを送信する(画面に表示するため)
+                        io.sockets.emit("HAL_turn_change", true);
+                        io.to(socket.id).emit("HAL_turn_change", false);
+                    }
+                    //ターンでない場合
+                    else{
+                        //ターンでないことを知らせる
+                        io.to(socket.id).emit("HAL_not_your_turn", "not YOUR turn");
+                    }
+                }
+                //プレイヤーが二人集まっていない場合
+                else{
+                    console.log("not enough player");
+                    //プレイヤーが二人集まっていないことを知らせる
+                    io.to(socket.id).emit("HAL_not_enough_player", "not enough player");
+                }
+            }
+        );
     }
 );
--- public/HighAndLow/HighAndLow.js
+++ public/HighAndLow/HighAndLow.js
@@ -27,6 +27,64 @@ function ready_clicked(){
 //正解の場合は正解と画面に表示し、不正解の場合は不正解と画面に表示する
 //その後、次のトランプを表示する
 function high_clicked(){
-    //サーバーにhighが押されたことを送信
-    socket.emit("HAL_high", "high");
+    //サーバにhighが押されたことを送信
+    socket.emit("HAL_pushed", "high");
 }
+
+//lowボタンについてもhighボタンと同様であるが、サーバにlowが押されたことを送信する
+function low_clicked(){
+    //サーバにlowが押されたことを送信
+    socket.emit("HAL_pushed", "low");
+}
+
+//HAL_current_card_nameを受信したときの処理
+//カードを表示する
+socket.on(
+    "HAL_current_card_name", (current_card_name)=>{
+        //HTML上のid = current_cardの画像を変更する
+        let current_card = document.getElementById("current_card");
+        current_card.src = "cards/" + current_card_name;
+    }
+);
+
+//HAL_turn_changeを受信したときの処理
+//ターンをお知らせするテキストをボタンの上に表示する
+socket.on(
+    "HAL_turn_change", (your_turn)=>{
+        //ターンをお知らせするテキストをボタンの上に表示するため、opacityを1にする
+        if(your_turn){
+            $("#turn_text").css("opacity", 1);
+        }
+        else{
+            $("#turn_text").css("opacity", 0);
+        }
+    }
+);
+//HAL_not_your_turnを受信したときの処理
+//ターンでないことをアラートでお知らせする
+socket.on(
+    "HAL_not_your_turn", (not_your_turn)=>{
+        alert(not_your_turn);
+    }
+);
+
+//HAL_iscorrectを受信したときの処理
+//正解であれば正解であることをアラートでお知らせする
+socket.on(
+    "HAL_iscorrect", (is_correct, player_name)=>{
+        if (is_correct){
+            alert(player_name + "さん　正解!");
+        }
+        else{
+            alert(player_name + "さん　不正解!");
+        }
+    }
+);
+
+//HAL_not_enough_playerを受信したときの処理
+//プレイヤーが足りないことをアラートでお知らせする
+socket.on(
+    "HAL_not_enough_player", (not_enough_players)=>{
+        alert(not_enough_players);
+    }
+);
